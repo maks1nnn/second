b8ace45d81be4a7afccdd5d9c4633bcc202e6667
maks1nnn <121863711+maks1nnn@users.noreply.github.com>
2024-03-31 18:48:58 +0300

    hometask 4 part 1 make first servise

---
+++ src/domain/post-servise.ts
@@ -0,0 +1,55 @@
+import { postRepository } from '../repositories/postMongo-repositories'
+import { blogRepository } from '../repositories/blogMongo-repositories'
+import { InputPostType, OutputPostDBType } from '../input-uotput-types/post-types'
+import { PostDBType } from '../db/post-db-type'
+//import {blogRepository} from '../repositories/blogs-repositories'
+import { ObjectId } from 'mongodb'
+import { BlogDBType } from '../db/blog-db-type'
+
+export const postServise = {
+    async createPosts(body: InputPostType) {
+        // Check if the associated blog exists
+
+        const blog = await blogRepository.findForOutput(new ObjectId(body.blogId));
+        if (!blog) {
+            // If the blog doesn't exist, return an error or throw an exception
+            throw new Error("Associated blog not found");
+        }
+
+        // Proceed with creating the post
+        const inputData = {
+            title: body.title,
+            shortDescription: body.shortDescription,
+            content: body.content,
+            blogId: body.blogId,
+            blogName: blog.name,
+            createdAt: (new Date()).toISOString()
+        };
+
+        return postRepository.createPosts(inputData)
+    },
+
+
+    async findPosts(id: ObjectId) {
+        return postRepository.findForOutput(id)
+    },
+
+
+    async getAllPosts() {
+        return postRepository.getAllPosts()
+
+    },
+
+
+    async updatePosts(id: ObjectId, body: InputPostType): Promise<boolean> {
+        return postRepository.updatePosts(id, body)
+
+
+
+    },
+
+
+    async deletePosts(id: ObjectId): Promise<boolean> {
+        return postRepository.deletePosts(id)
+    },
+}
---
+++ src/repositories/postMongo-repositories.ts
@@ -0,0 +1,96 @@
+import { postCollection } from '../db/mongo-db'
+import { blogCollection } from '../db/mongo-db'
+import { InputPostType, OutputPostDBType } from '../input-uotput-types/post-types'
+import { PostDBType } from '../db/post-db-type'
+//import {blogRepository} from '../repositories/blogs-repositories'
+import { ObjectId } from 'mongodb'
+import { BlogDBType } from '../db/blog-db-type'
+
+export const postRepository = {
+    async createPosts(inputData: InputPostType) {
+
+
+        try {
+            const insertInfo = await postCollection.insertOne(inputData);
+            console.log(insertInfo);
+            return insertInfo.insertedId;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    },
+
+
+    async findPosts(id: ObjectId) {
+        const post = await postCollection.findOne({ _id: id })
+        if (post) {
+            return post
+        } else { return null }
+    },
+
+    async findForOutput(id: ObjectId) {
+        const post = await this.findPosts(id)
+        if (post !== null) {
+            return this.mapToOutput(post as PostDBType)
+        } else { return false }
+    },
+
+
+    mapToOutput(post: PostDBType) {
+        return {
+            id: post._id.toString(),
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            blogName: post.blogName,
+            createdAt: post.createdAt,
+            blogId: post.blogId,
+
+        }
+    },
+
+    async getAllPosts() {
+        const posts = await postCollection.find({}).toArray()
+        return posts.map(post => ({
+            id: post._id.toString(),
+            title: post.title,
+            shortDescription: post.shortDescription,
+            content: post.content,
+            blogName: post.blogName,
+            createdAt: post.createdAt,
+            blogId: post.blogId,
+
+        }))
+
+    },
+
+
+    async updatePosts(id: ObjectId, body: InputPostType): Promise<boolean> {
+        const result = await postCollection.updateOne({ _id: id }, {
+            $set: {
+                shortDescription: body.shortDescription,
+                title: body.title,
+                content: body.content,
+                blogId: body.blogId,
+            }
+
+        })
+        return result.modifiedCount === 1
+
+
+        /* const upPost = db.posts.find(p => p.id === id)
+         if (upPost) {
+             upPost.title = body.title
+             upPost.shortDescription = body.shortDescription
+             upPost.content = body.content
+             upPost.blogId = body.blogId
+             return true
+         } else { return false }*/
+    },
+
+
+    async deletePosts(id: ObjectId): Promise<boolean> {
+        const result = await postCollection.deleteOne({ _id: id })
+        return result.deletedCount === 1
+    },
+}
