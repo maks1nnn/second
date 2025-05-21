8f96b04c2573248f49aab45a7dbe56006959488a
maks1nnn <121863711+maks1nnn@users.noreply.github.com>
2025-05-22 00:11:27 +0300

    test security

--- __tests__/helpers/req.ts
+++ __tests__/helpers/req.ts
@@ -2,4 +2,5 @@ import {app} from '../../src/app'
 import {agent} from 'supertest'
 import TestAgent from 'supertest/lib/agent'
 import { Test } from 'supertest'
+
 export const req: TestAgent<Test> = agent(app)
