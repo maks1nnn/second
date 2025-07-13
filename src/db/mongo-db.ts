be7600e93141f9b2d4e6bd5ce9d3baa5eac2f582
maks1nnn <121863711+maks1nnn@users.noreply.github.com>
2025-07-13 22:56:54 +0300

    11

--- src/db/mongo-db.ts
+++ src/db/mongo-db.ts
@@ -7,15 +7,16 @@ console.log(SETTINGS.MONGO_URL)
 
 //const client: MongoClient = new MongoClient(SETTINGS.MONGO_URL)
 
-export const db : Db = client.db(SETTINGS.DB_NAME);
+//export const db : Db = client.db(SETTINGS.DB_NAME);
 
-export const postCollection  = db.collection (SETTINGS.POST_COLLECTION_NAME)
-export const blogCollection  = db.collection (SETTINGS.BLOG_COLLECTION_NAME)
-export const userCollection   = db.collection (SETTINGS.USER_COLLECTION_NAME)
-export const authCollection = db.collection(SETTINGS.AUTH_COLLECTION_NAME)
-export const commentCollection = db.collection(SETTINGS.COMMENT_COLLECTION_NAME)
-export const ipControlCollection = db.collection(SETTINGS.IP_COLLECTION_NAME)
-export const rateCollection = db.collection(SETTINGS.RATE_COLLECTION_NAME)
+
+//export const postCollection  = db.collection (SETTINGS.POST_COLLECTION_NAME)
+//export const blogCollection  = db.collection (SETTINGS.BLOG_COLLECTION_NAME)
+//export const userCollection   = db.collection (SETTINGS.USER_COLLECTION_NAME)
+//export const authCollection = db.collection(SETTINGS.AUTH_COLLECTION_NAME)
+//export const commentCollection = db.collection(SETTINGS.COMMENT_COLLECTION_NAME)
+//export const ipControlCollection = db.collection(SETTINGS.IP_COLLECTION_NAME)
+//export const rateCollection = db.collection(SETTINGS.RATE_COLLECTION_NAME)
 
 export const connectionToDB = async () => {
     try {
@@ -25,7 +26,7 @@ export const connectionToDB = async () => {
     
     }catch(e) {
         console.log(e)
-        await client.close()
+        await mongoose.disconnect()
         return false
     }
 }
